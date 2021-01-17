821932878bde22ea1e2436731bf64b7b51ed2e9e
Alexander Todorov <aleksandar9999a@gmail.com>
2021-01-17 13:00:05 +0200

    Rename component, add layout

--- src/pages/Home/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-export const Home: React.FC = () => (
-  <div>
-    <h1>Listen</h1>
-  </div>
-);
---
+++ src/pages/Listen/Listen.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+// Components
+import {
+  IonCol,
+  IonFabButton,
+  IonGrid,
+  IonIcon,
+  IonImg,
+  IonRange,
+  IonRow,
+  IonSlide,
+  IonSlides
+} from '@ionic/react';
+
+// Icons
+import {
+  play,
+  chevronBack,
+  chevronForward
+} from 'ionicons/icons';
+
+import { observer } from 'mobx-react';
+
+
+export const Listen = observer(() => (
+  <IonGrid>
+    <IonRow>
+      {window.innerWidth > 700 && (
+        <IonCol sizeLg="6">
+          Playlist
+          </IonCol>
+      )}
+
+      <IonCol sizeLg="6">
+        <IonSlides className="ion-margin-bottom" pager={false} style={{ maxHeight: '50vh' }}>
+          <IonSlide>
+            <IonImg src="assets/images/unknown.png" />
+          </IonSlide>
+        </IonSlides>
+        
+        <div className="ion-text-center ion-padding-top">
+          <h2>Track Name</h2>
+          
+          <p>Track Artist</p>
+        </div>
+
+        <div className="ion-padding-start ion-padding-end ion-margin-bottom">
+          <IonRange
+            max={100}
+            color="success"
+          >
+          </IonRange>
+        </div>
+
+        <div className="d-flex ion-align-items-center ion-justify-content-center ion-padding-top ion-padding-bottom">
+          <IonFabButton
+            className="ion-margin-end"
+            color="light"
+          >
+            <IonIcon icon={chevronBack} />
+          </IonFabButton>
+
+          <IonFabButton
+            className="ion-margin-end dark"
+            color="light"
+          >
+            <IonIcon icon={play} />
+          </IonFabButton>
+
+          <IonFabButton color="light">
+            <IonIcon icon={chevronForward} />
+          </IonFabButton>
+        </div>
+      </IonCol>
+    </IonRow>
+  </IonGrid>
+))
