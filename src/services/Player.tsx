926db2d11d47a29254120c073eea037488cc3451
Alexander Todorov <aleksandar9999a@gmail.com>
2021-01-18 07:36:21 +0200

    Added basic infrastructure

--- src/services/Player.tsx
+++ src/services/Player.tsx
@@ -1,5 +1,54 @@
-import { injectable } from "inversify";
+import { history } from './../App';
+import { injectable } from 'inversify';
+import { action, makeObservable, observable } from 'mobx';
+import { ITrack } from '../interfaces/interfaces';
+import { Plugins } from '@capacitor/core';
+const { Storage } = Plugins;
+
 
 @injectable()
 export class Player {
+  @observable
+  isStarted: boolean = false;
+
+  @observable
+  progres: number = 0;
+
+  @observable
+  track: ITrack = {
+    picture: '',
+    artist: '',
+    title: '',
+    url: ''
+  };
+
+  @observable
+  tracks: ITrack[] = [];
+
+  constructor () {
+    makeObservable(this);
+  }
+
+  @action
+  loadTracks () {
+    Storage.get({ key: 'music' }).then(data => {
+      if (!data.value) {
+        history.push('/folders');
+      }
+    })
+  }
+
+  @action
+  handlePlay () {
+    this.isStarted = !this.isStarted;
+  }
+
+  @action
+  handleNext () {}
+
+  @action
+  handlePrev () {}
+
+  @action
+  handleSlideChange () {}
 }
