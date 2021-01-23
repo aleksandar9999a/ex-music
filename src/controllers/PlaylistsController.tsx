75a46bc45325157f26162a3d8aa100fd3ae0d470
Alexander Todorov <aleksandar9999a@gmail.com>
2021-01-23 21:03:57 +0200

    Added actions, playlists and etc

--- src/controllers/PlaylistsController.tsx
+++ src/controllers/PlaylistsController.tsx
@@ -2,6 +2,8 @@ import { inject, injectable } from 'inversify';
 import { action, makeObservable, observable } from 'mobx';
 import { type } from '../Types';
 import { StorageService } from '../services/Storage';
+import { IPlaylist } from '../interfaces/interfaces';
+import { Subscription } from 'rxjs';
 
 
 @injectable()
@@ -9,24 +11,82 @@ export class PlaylistsController {
   @inject(type.StorageService) private storageService!: StorageService
 
   @observable
+  playlists: IPlaylist[] = [];
+
+  @observable
   isOpenPopover: boolean = false;
+
+  @observable
+  playlistName: string = '';
+
+  @observable
+  playlistImage: string = '';
+
+  playlistImageFile?: string | null;
+
+  playlistSubscriber!: Subscription;
 
   constructor () {
     makeObservable(this);
   }
-  
+
   @action
-  addPlaylist () {
-    console.debug('add playlist')
+  setPlaylistImage = (file: File) => {
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      this.playlistImageFile = e.target.result;
+    }
+    
+    const url = URL.createObjectURL(file);
+    this.playlistImage = url;
+
+    reader.readAsDataURL(file);
   }
 
   @action
-  openPopover () {
+  setPlaylist = (data: IPlaylist[]) => {
+    this.playlists = data
+  }
+
+  @action
+  subscribeForPlaylists () {
+    this.playlistSubscriber = this.storageService.playlists.subscribe(this.setPlaylist);
+  }
+
+  @action
+  unsubscribeForPlaylists () {
+    this.playlistSubscriber.unsubscribe();
+  }
+
+  @action
+  handlePlaylistName = (e: any) => {
+    this.playlistName = e.target.value;
+  }
+  
+  @action
+  addPlaylist = () => {
+    if (!this.playlistName) {
+      return;
+    }
+
+    return this.storageService.savePlaylist(this.playlistName, this.playlistImageFile)
+      .then(data => {
+        this.playlistName = ''
+        this.playlistImage = '';
+        this.playlistImageFile = null;
+        this.closePopover();
+      })
+  }
+
+  @action
+  openPopover = () => {
     this.isOpenPopover = true
   }
 
   @action
-  closePopover () {
+  closePopover = () => {
     this.isOpenPopover = false
+    this.playlistName = ''
   }
 }
--- src/pages/Playlists/PlaylistsView.tsx
+++ src/pages/Playlists/PlaylistsView.tsx
@@ -1,35 +1,104 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { observer } from 'mobx-react';
 
 // Cotrollers
 import { PlaylistsController } from '../../controllers/PlaylistsController';
 
 // Components
 import {
+  IonAvatar,
+  IonButton,
   IonFab,
   IonFabButton,
   IonHeader,
   IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPopover,
   IonTitle,
   IonToolbar
 } from '@ionic/react';
 
+import { ImageUpload } from '../../components/ImageUploader/ImageUploader';
+
 // Icons
-import { add } from 'ionicons/icons';
-import { observer } from 'mobx-react';
+import {
+  add,
+  checkmarkOutline
+} from 'ionicons/icons';
 
 
-export const PlaylistsView =  observer(({ playlistsController }: { playlistsController: PlaylistsController }) => (
-  <div>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle className="text-center">Playlists</IonTitle>
-      </IonToolbar>
-    </IonHeader>
+export const PlaylistsView =  observer(({ playlistsController }: { playlistsController: PlaylistsController }) => {
+  useEffect(() => {
+    playlistsController.subscribeForPlaylists();
 
-    <IonFab vertical="top" horizontal="end" slot="fixed" style={{ top: '26px' }}>
-      <IonFabButton onClick={playlistsController.addPlaylist}>
-        <IonIcon icon={add} />
-      </IonFabButton>
-    </IonFab>
-  </div>
-))
+    return () => {
+      playlistsController.unsubscribeForPlaylists();
+    }
+  })
+
+  return (
+    <div className="h-100">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle className="text-center">Playlists</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonFab
+        vertical="top"
+        horizontal="end"
+        slot="fixed" style={{ top: '26px' }}
+      >
+        <IonFabButton onClick={playlistsController.openPopover}>
+          <IonIcon icon={add} />
+        </IonFabButton>
+      </IonFab>
+
+      <IonList className="h-100">
+        {playlistsController.playlists.map(playlist => (
+          <IonItem key={playlist.id}>
+            <IonAvatar className="mr-3">
+              <img src={playlist.image as string || '/assets/images/unknown.png'} />
+            </IonAvatar>
+
+            <IonLabel>{playlist.name}</IonLabel>
+          </IonItem>
+        ))}
+      </IonList>
+
+      <IonPopover
+        isOpen={playlistsController.isOpenPopover}
+        onDidDismiss={playlistsController.closePopover}
+      >
+        <div>
+          <ImageUpload
+            src={playlistsController.playlistImage}
+            onChange={playlistsController.setPlaylistImage}
+          />
+        </div>
+
+        <div className="d-flex align-items-center">
+          <IonItem>
+            <IonLabel position="floating">Playlist Name</IonLabel>
+
+            <IonInput
+              value={playlistsController.playlistName}
+              onIonChange={playlistsController.handlePlaylistName}
+            />
+          </IonItem>
+
+          <IonButton
+            className="ml-2 mr-2 text-success"
+            fill="clear"
+            onClick={playlistsController.addPlaylist}
+          >
+            <IonIcon icon={checkmarkOutline} />
+          </IonButton>
+        </div>
+      </IonPopover>
+    </div>
+  )
+})
