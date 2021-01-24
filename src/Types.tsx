e636928a6f8a99a8344f7c4339b89ffa1afd56e6
Alexander Todorov <aleksandar9999a@gmail.com>
2021-01-24 12:46:57 +0200

    Added new type

--- src/Types.tsx
+++ src/Types.tsx
@@ -1,5 +1,6 @@
 export const type = {
   PlaylistsController: Symbol.for('PlaylistsController'),
+  PlaylistController: Symbol.for('PlaylistController'),
   PlayerController: Symbol.for('PlayerController'),
   StorageService: Symbol.for('StorageService'),
   Router: Symbol.for('Router')
