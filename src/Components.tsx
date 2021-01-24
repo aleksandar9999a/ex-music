1ae337c600315219405f61faf0e404f4de41700d
Alexander Todorov <aleksandar9999a@gmail.com>
2021-01-24 12:46:03 +0200

    Added components container

---
+++ src/Components.tsx
@@ -0,0 +1,9 @@
+import { PlayerView } from './pages/Player/PlayerView';
+import { PlaylistsView } from './pages/Playlists/PlaylistsView';
+import { PlaylistView } from './pages/Playlist/PlaylistView';
+
+export const components = {
+  PlayerView,
+  PlaylistsView,
+  PlaylistView
+}
