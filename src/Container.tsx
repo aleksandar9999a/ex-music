d283173163be3aabcbc3a2cf90afb9ee5cda2397
Alexander Todorov <aleksandar9999a@gmail.com>
2021-01-24 12:46:38 +0200

    Registered new controller

--- src/Container.tsx
+++ src/Container.tsx
@@ -4,11 +4,13 @@ import { Router } from "./services/Router";
 import { PlayerController } from "./controllers/PlayerCotroller";
 import { StorageService } from "./services/Storage";
 import { PlaylistsController } from "./controllers/PlaylistsController";
+import { PlaylistController } from "./controllers/PlaylistController";
 
 
 const container = new Container();
 
 container.bind<PlaylistsController>(type.PlaylistsController).to(PlaylistsController);
+container.bind<PlaylistController>(type.PlaylistController).to(PlaylistController);
 container.bind<StorageService>(type.StorageService).to(StorageService);
 container.bind<PlayerController>(type.PlayerController).to(PlayerController);
 container.bind<Router>(type.Router).to(Router);
